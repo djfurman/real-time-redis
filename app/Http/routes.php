692fbf3f68a32e8dbaeaa47fd887a5329d780cbc
Furmanator <dfurman@kfgisit.com>
2016-01-22 23:17:46 -0500

    Provides working Laravel, Node, Socket, Redis functionality to emit real time events

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,6 +21,10 @@ Route::get('/', function () {
 
     // 1. Publish Event with Redis
 
+    /*
+     * 1.1 Setup some dummy data; this would be a full event or object model in real life
+     */
+
     $data = [
         'event' => 'UserSignedUp',
         'data' => [
@@ -28,13 +32,19 @@ Route::get('/', function () {
         ]
     ];
 
+    /*
+     * 1.2 Publish the event to the Redis 'test-channel' by JSON encoding the data into Redis
+     */
     Redis::publish('test-channel', json_encode($data));
 
     // 2. Node.js + Redis subscribes to even
     /*
      * Node will run from the project root folder's 'socket.js' file to
      *   Read from Redis
-     *   Log to the console
+     *   Broadcast the event on Socket.io
+     *
+     * See the socket.js file in the project root
+     * This file must be triggered with `node socket.js` in order to setup the node server
      */
 
     // 3. Use Socket.io to emit to all clients
